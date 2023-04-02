4bd1540878678d8422fd7dafd577ef83f710874b
danilsnegurov <d.sniegurov@foodtech.team>
2023-04-02 15:16:18 +0300

    second commit

---
+++ src/app/feature/messenger/messenger.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MessengerComponent } from './messenger/messenger.component';
+
+
+
+@NgModule({
+  declarations: [
+    MessengerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MessengerModule { }
